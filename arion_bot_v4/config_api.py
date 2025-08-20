ee6f13f60a4b8ccdb9430d0fc955ef07a5df9ac0
juliojgcarvalho <juliocanticos@gmail.com>
2025-08-20 07:26:59 -0300

    Update config_api.py

--- arion_bot_v4/config_api.py
+++ arion_bot_v4/config_api.py
@@ -1,5 +1,5 @@
 # config_api.py
 # Preencha suas chaves API da Binance abaixo
 
-API_KEY = oXYBnDjYwvjkvz00Xoz9iZAkKpQXL4HBCrnjv7kFE2t2zG0iqtZ12zJN2y84qqDH
-API_SECRET = TMXJ1Dj1wDH9ZMrJZHixVnO7ODKG76w29t7S9WpnqqqizgmwNT3YuFzVgbKAZMs6
+API_KEY = "oXYBnDjYwvjkvz00Xoz9iZAkKpQXL4HBCrnjv7kFE2t2zG0iqtZ12zJN2y84qqDH"
+API_SECRET = "TMXJ1Dj1wDH9ZMrJZHixVnO7ODKG76w29t7S9WpnqqqizgmwNT3YuFzVgbKAZMs6"
