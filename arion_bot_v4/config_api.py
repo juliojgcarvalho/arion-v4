1b8bdfcdf084fb507f7bf3febe7eff4ecb56485b
juliojgcarvalho <juliocanticos@gmail.com>
2025-08-20 13:39:04 -0300

    Update config_api.py

--- arion_bot_v4/config_api.py
+++ arion_bot_v4/config_api.py
@@ -1,7 +1,7 @@
 # config_api.py
 # Configurações de acesso à Binance API para leitura de dados públicos
 
-API_BASE = "https://data.binance.com"
+API_BASE = "https://fapi.binance.com"
 
 # Substitua abaixo com suas chaves reais apenas se for operar de verdade
 API_KEY = "CXHLAuxm0H2dF5zWInOh0GE1zdc9kcSDDmE2vsZGvoILF7qsHNHl2ZZrHr7BOJ4w"
