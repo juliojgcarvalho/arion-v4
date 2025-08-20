4b7388f7312e6bbf69c4f837795e1234da1f5834
juliojgcarvalho <juliocanticos@gmail.com>
2025-08-20 08:40:10 -0300

    Update config_api.py

--- arion_bot_v4/config_api.py
+++ arion_bot_v4/config_api.py
@@ -1,3 +1,7 @@
 # config_api.py
-API_KEY = "oXYBnDjYwvjkvz00Xoz9iZAkKpQXL4HBCrnjv7kFE2t2zG0iqtZ12zJN2y84qqDH"
-API_SECRET = "TMXJ1Dj1wDH9ZMrJZHixVnO7ODKG76w29t7S9WpnqqqizgmwNT3YuFzVgbKAZMs6t"
+
+API_BASE = "https://fapi.binance.com/fapi/v1/klines"
+
+API_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ArionBot/1.0"
+}
